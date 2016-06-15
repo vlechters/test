0d8e0b257b6060218a6f61b100b936f8f6d20a7b
coldmow <mohammedb060@gmail.com>
2016-06-15 04:15:40 +0200

    removed excessive files

--- src_NO/cookie.php
+++
@@ -1,5 +0,0 @@
-<?php
-$cookie_name = "user";
-$cookie_value = "cms_gebruiker";
-setcookie($cookie_name, $cookie_value, time() + (899), "/");
-?>
--- src_NO/forms_inzien.php
+++
@@ -1,7 +0,0 @@
-
-<html>
-	<head></head>
-	<body>
-		<iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;height=400&amp;wkst=2&amp;bgcolor=%23ffffff&amp;src=masoumiprojects%40gmail.com&amp;color=%2329527A&amp;ctz=Europe%2FAmsterdam" style="border-width:0" width="600" height="400" frameborder="0" scrolling="no"></iframe>
-	</body>
-</html>

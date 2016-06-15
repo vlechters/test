00b22090574183545d572c5e10ab172ab2b40569
coldmow <mohammedb060@gmail.com>
2016-06-15 02:54:19 +0200

    Front end style bijgewerkt
    gebruiker interface bugs opgelost
    animaties verbeterd
    & vergelijkbare css bewerkingen

--- index.php
+++ index.php
@@ -20,32 +20,12 @@ require 'connecties\artikeldatabase.php';
 	<script type="text/javascript" charset="utf-8">
 		$(document).ready(function(){
 
-
-			click = true;
-			$('.hamburger').click(function(){
-				if(click){
-					$('#sideMenu').css('width','300px');
-					$('body').css('margin-left','300px');
+			$('.hamburger,#sideMenuBar').click(function(){
+					$('#sideMenu').toggleClass('open');
+					$('#sideMenuBar').toggleClass('open');
 					$('body').toggleClass('open');
-					click = false;
-				}else{
-					$('#sideMenu').css('width','0');
-					$('body').css('margin-left','0');
-					click = true;
-				}
 			});
 
-			if(click==false){
-				$('body').click(function(){
-					$('#sideMenu').css('width','0');
-					$('body').css('margin-left','0');
-					click = true;
-				});
-				
-			};
-
-
-			
 			
 	
 		});
@@ -59,7 +39,8 @@ require 'connecties\artikeldatabase.php';
 
 </head>
 									<!-- BEGIN-BODY -->
-<body id="page">
+<body>
+
 	<div class="top-bar">
 		<div id="sideMenu">
 			<ul>
@@ -69,6 +50,7 @@ require 'connecties\artikeldatabase.php';
 				<a href="">    <li><i class="fa fa-cog fa-2x"></i>&nbsp;&nbsp;Instellingen</li>    </a>
 			</ul>
 		</div>
+		<div id="sideMenuBar"></div>
 
 		<div class="banner">
 
@@ -113,80 +95,81 @@ require 'connecties\artikeldatabase.php';
 	
 
 	
-	<div class="content">
+	<div class="gig">
+		<div class="content">
 
-		<div class="slider-container">		<!-- BEGIN FOTO-SLIDER-->
+			<div class="slider-container">		<!-- BEGIN FOTO-SLIDER-->
 
-			<div class="cycle-slideshow"
-			data-cycle-auto-height="16:9"
-			data-cycle-loader="wait"
-			data-cycle-fx="scrollHorz"
-			data-cycle-speed="1000"
+				<div class="cycle-slideshow"
+				data-cycle-auto-height="16:9"
+				data-cycle-loader="wait"
+				data-cycle-fx="scrollHorz"
+				data-cycle-speed="1000"
 
-			data-cycle-prev="#Bl"
-			data-cycle-next="#Br"
-			>
-			<!-- <div class="cycle-pager"></div> -->
-				<img src="wallpaper1.jpg">
-				<img src="wallpaper2.jpg">
-				<img src="wallpaper3.jpg">
+				data-cycle-prev="#Bl"
+				data-cycle-next="#Br"
+				>
+				<!-- <div class="cycle-pager"></div> -->
+					<img src="wallpaper1.jpg">
+					<img src="wallpaper2.jpg">
+					<img src="wallpaper3.jpg">
+				</div>
+
+				<a href=# class="SlideButton" id="Bl">&lang;</a>
+				<a href=# class="SlideButton" id="Br">&rang;</a>
 			</div>
-
-			<a href=# class="SlideButton" id="Bl">&lang;</a>
-			<a href=# class="SlideButton" id="Br">&rang;</a>
-		</div>
-	
-		<br>
-		<br>
-		<br>								<!-- BEGIN HOOFD-INHOUD WEBSITE -->
-		<div class="content1">
-			<h2 onclick="OpenMenu()">Welkom op de website van de VERENIGING VAN VLECHTERS</h2>
-			<img src="logo.gif" alt="logo">
-				
-			<p>De Vereniging van Vlechters is een vereniging, die contacten tussen mensen met belangstelling voor het vlechten wil stimuleren en onderhouden.</p>
-			<p>Deze onderlinge contacten wil onze vereniging bevorderen door deze website, door het periodiek uitgeven van een Vlechtbulletin en door het organiseren van contactdagen.</p>
-		</div>
 		
-
-		<div id="Boxez">
-			<div class="boxes" id="box1">
-				<h3>Litora torquent per conubia nostra.</h3>
-
-					</p>
-
-
-				<div class="auteur">Mohammad Masoumi</div><div class="datum"> 13-05-2016</div><!-- auteur en datum aangemaakt -->
+			<br>
+			<br>
+			<br>								<!-- BEGIN HOOFD-INHOUD WEBSITE -->
+			<div class="content1">
+				<h2 onclick="OpenMenu()">Welkom op de website van de VERENIGING VAN VLECHTERS</h2>
+				<img src="logo.gif" alt="logo">
+					
+				<p>De Vereniging van Vlechters is een vereniging, die contacten tussen mensen met belangstelling voor het vlechten wil stimuleren en onderhouden.</p>
+				<p>Deze onderlinge contacten wil onze vereniging bevorderen door deze website, door het periodiek uitgeven van een Vlechtbulletin en door het organiseren van contactdagen.</p>
 			</div>
-			<div class="boxes" id="box2">
-				<h3>Dolor sit amet, consectetur, adipisci.</h3>
-				<p><?php
-					// Rows ophalen van DB 
-					$conn = ArtikelDatabase::getConnection();
-					$q = "SELECT auteur,afbeelding_url,titel,artikel_inhoud,datum_aangemaakt FROM artikels";
-					$stmt = $conn->prepare( $q );
-					$stmt->execute();
+			
 
-					while($row = $stmt->fetch()){
-						echo 	"<tr>";
-						echo	"<td style='color:#e20363; text-align:center; display:none'><input type='text' value='{$row['auteur']}' name='id' readonly></td>";
-						echo	"<td><input   type='text' value='{$row['afbeelding_url']}' name='afbeelding_url'></td>";
-						echo	"<td><input   type='text' value='{$row['titel']}' name='titel' ></td>";
-						echo	"<td ><input  type='text' value='{$row['artikel_inhoud']}' name='artikel_inhoud' ></td>";
-						echo	"<td ><input  type='text' value='{$row['datum_aangemaakt']}' name='datum_aangemaakt' ></td>";
-					}
-					?></p>
+			<div id="Boxez">
+				<div class="boxes" id="box1">
+					<h3>Litora torquent per conubia nostra.</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum quos placeat delectus mollitia magnam adipisci eum neque voluptas, iusto reprehenderit voluptate obcaecati excepturi dolor sint cumque hic officia impedit inventore. Ipsam magni odio atque a. Cum eaque illum nulla aliquid, vitae blanditiis dicta laudantium officiis voluptatem amet soluta delectus neque voluptatum cupiditate vero excepturi reprehenderit, ipsa porro id! Quo magni maiores suscipit adipisci excepturi cum consequatur perferendis odio temporibus. Repudiandae ratione natus maxime praesentium, quo, quas, esse inventore velit, nihil aperiam deserunt ipsa atque a ea blanditiis illum. Quasi neque ullam delectus similique, nam voluptas non vitae. Illo, tempore, maiores?</p>
+
+					<div class="auteur">Mohammad Masoumi</div><div class="datum"> 13-05-2016</div><!-- auteur en datum aangemaakt -->
+				</div>
+				<div class="boxes" id="box2">
+					<h3>Dolor sit amet, consectetur, adipisci.</h3>
+					<p>
+						<?php
+							// Rows ophalen van DB 
+							$conn = ArtikelDatabase::getConnection();
+							$q = "SELECT auteur,afbeelding_url,titel,artikel_inhoud,datum_aangemaakt FROM artikels";
+							$stmt = $conn->prepare( $q );
+							$stmt->execute();
+		
+							while($row = $stmt->fetch()){
+								echo 	"<tr>";
+								echo	"<td style='color:#e20363; text-align:center; display:none'><input type='text' value='{$row['auteur']}' name='id' readonly></td>";
+								echo	"<td><input   type='text' value='{$row['afbeelding_url']}' name='afbeelding_url'></td>";
+								echo	"<td><input   type='text' value='{$row['titel']}' name='titel' ></td>";
+								echo	"<td ><input  type='text' value='{$row['artikel_inhoud']}' name='artikel_inhoud' ></td>";
+								echo	"<td ><input  type='text' value='{$row['datum_aangemaakt']}' name='datum_aangemaakt' ></td>";
+							}
+							?>
+						</p>
+				</div>
+				<div class="boxes" id="box3">
+					<h3>Arcu nec dapibus. Vivamus et ligula nec.</h3>
+					<p>Pellentesque accumsan porta lacus, non aliquet erat venenatis at. Aenean varius, justo sit amet cursus semper, enim nulla egestas ante, sit amet auctor metus ligula sed quam. In convallis faucibus quam et dapibus. Suspendisse feugiat lorem neque, ut porta neque faucibus ac. Nullam varius convallis malesuada. Quisque consequat luctus arcu nec dapibus. Vivamus et ligula nec urna posuere ultrices pretium vitae ipsum. Maecenas at laoreet tortor.</p>
+				</div>
+				<div class="boxes" id="box4">
+					<h3>Dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</h3>
+					<p>In felis justo, laoreet a tempus at, accumsan a sem. Suspendisse laoreet quis libero quis luctus. Praesent pulvinar pellentesque felis, in gravida dui. Etiam volutpat dignissim enim, et facilisis neque molestie a. Nam auctor velit sit amet velit pharetra gravida. Quisque vulputate lacus non arcu dignissim consectetur. Etiam vitae placerat est, sit amet efficitur dolor. Suspendisse aliquet libero quam, non venenatis eros sollicitudin sit amet. Vivamus auctor neque quis metus mattis venenatis.</p>
+				</div>
 			</div>
-			<div class="boxes" id="box3">
-				<h3>Arcu nec dapibus. Vivamus et ligula nec.</h3>
-				<p>Pellentesque accumsan porta lacus, non aliquet erat venenatis at. Aenean varius, justo sit amet cursus semper, enim nulla egestas ante, sit amet auctor metus ligula sed quam. In convallis faucibus quam et dapibus. Suspendisse feugiat lorem neque, ut porta neque faucibus ac. Nullam varius convallis malesuada. Quisque consequat luctus arcu nec dapibus. Vivamus et ligula nec urna posuere ultrices pretium vitae ipsum. Maecenas at laoreet tortor.</p>
-			</div>
-			<div class="boxes" id="box4">
-				<h3>Dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.</h3>
-				<p>In felis justo, laoreet a tempus at, accumsan a sem. Suspendisse laoreet quis libero quis luctus. Praesent pulvinar pellentesque felis, in gravida dui. Etiam volutpat dignissim enim, et facilisis neque molestie a. Nam auctor velit sit amet velit pharetra gravida. Quisque vulputate lacus non arcu dignissim consectetur. Etiam vitae placerat est, sit amet efficitur dolor. Suspendisse aliquet libero quam, non venenatis eros sollicitudin sit amet. Vivamus auctor neque quis metus mattis venenatis.</p>
-			</div>
+
 		</div>
-
 	</div>
-	
 </body>
 </html>

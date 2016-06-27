9ebb83e2d979017834725b270f9d69c26938fe43
coldmow <mohammedb060@gmail.com>
2016-06-27 08:30:45 +0200

    Top en left menu in php geschreven om te includen in andere pagina's

--- artikelen.php
+++ artikelen.php
@@ -44,59 +44,10 @@ if (!empty($_GET['artikel_id'])) {
 									<!-- BEGIN-BODY -->
 <body>
 
-	<div class="top-bar">
-		<div id="sideMenu">
-			<ul>
-				<a href="index.php">    <li><i class="fa fa-home fa-2x"></i>&nbsp;&nbsp;Home</li>    </a>
-				<a href="informatie.php">    <li><i class="fa fa-info-circle fa-2x"></i>&nbsp;&nbsp;Informatie</li>    </a>
-				<a href="nieuws.php">    <li><i class="fa fa-newspaper-o fa-2x"></i>&nbsp;&nbsp;Nieuws</li>    </a>
-				<a href="">    <li><i class="fa fa-cog fa-2x"></i>&nbsp;&nbsp;Instellingen</li>    </a>
-			</ul>
-		</div>
-		<div id="sideMenuBar"></div>
-
-		<div class="banner">
-
-			<i class="fa fa-bars fa-2x hamburger"></i>
-			<!-- <i class="fa fa-bars fa-2x hamburger" onclick="OpenMenu()"></i> -->
-
-			<a href="src/index.php"><div class="begroeting">
-							Log In <i class="fa fa-chevron-circle-down"></i>
-						</div></a>
-			<ul class="menu">
-				<a href="index.php">
-					<li class="home">
-						<i class="fa fa-home fa-3x"></i>
-						<br>Home
-					</li> | 
-				</a>
-
-				<a href="informatie.php">
-					<li >
-						<i class="fa fa-info-circle fa-3x"></i>
-						<br>Informatie
-					</li> | 
-				</a>
-
-				<a href="artikelen.php">
-					<li >
-						<i class="fa fa-newspaper-o fa-3x"></i>
-						<br>Artikelen
-					</li> | 
-				</a>
-				
-				<a href="#">
-					<li class="settings">
-						<i class="fa fa-cog fa-3x"></i>
-						<br>Instellingen
-					</li>
-				</a>
-			</ul>
-			
-		</div>
-	</div>
+	<?php
+	require('header+side_menu.php');
+	?>
 	
-
 	
 	<div class="gig">
 		<div class="content">
